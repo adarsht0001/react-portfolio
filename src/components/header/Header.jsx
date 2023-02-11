16ec0ce37a0d3125df78e9aa0f28f819d88f9381
adarsht00001 <adarsht00001@gmail.com>
2023-02-12 02:43:55 +0530

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,11 +7,12 @@ const Header = () => {
   return (
     <header>
         <div className="container header_container">
-            <h5>Hello I'm</h5>
+            <h5>Hello, I'm</h5>
             <h1>Adarsh</h1>
             <h5 className="text-light">MERN STACK DEVELOPER</h5>
             <CTA/>
             <HeaderSocials/>
+            <a href="#contact" className='scroll_down'>Scroll Down</a>
         </div>
     </header>
   )
