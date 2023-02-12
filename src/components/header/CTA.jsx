e24e7d738b3feceb196db80bf17ce084f39624e0
adarsht00001 <adarsht00001@gmail.com>
2023-02-12 13:34:40 +0530

    cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from "../../assets/cv.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="{CV}" download={"CV"} className="btn">
+      <a href={CV} download={"CV"} className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
