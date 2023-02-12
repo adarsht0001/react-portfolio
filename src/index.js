222710ecde64a51c92c225938756dfb6ae5d01b1
adarsht00001 <adarsht00001@gmail.com>
2023-02-12 17:38:04 +0530

    navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom/client';
 import App from './App'
 import './index.css'
 
-ReactDom.render(<App/>,document.querySelector("#root"))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  
