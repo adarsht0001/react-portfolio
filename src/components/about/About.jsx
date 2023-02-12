cf7fe72459c2eaf9562f5fe2a893f304593e831b
adarsht00001 <adarsht00001@gmail.com>
2023-02-12 17:15:38 +0530

    navbar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>A</div>
+    <section id='about'>A</section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome,AiOutlineUser} from 'react-icons/ai'
+import { useState } from 'react'
 
 const Nav = () => {
+  const[activeNav,setActiveNav]=useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={()=>setActiveNav('#')}  className={activeNav==='#'?'active':''}><AiOutlineHome/></a>
+      <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav==='#about'?'active':''}><AiOutlineUser/></a>
+      <a href="#Experience"><AiOutlineHome/></a>
+      <a href="#s"><AiOutlineHome/></a>
+      <a href="#s"><AiOutlineHome/></a>
+    </nav>
   )
 }
 
