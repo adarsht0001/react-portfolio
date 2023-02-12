5e31ef58a0ebb064efaec1b51e8aa6707ea1b985
adarsht00001 <adarsht00001@gmail.com>
2023-02-12 19:32:16 +0530

    experience

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from './components/about/About'
+import Experience from './components/experince/Experience'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 
@@ -9,6 +10,7 @@ const App = () => {
         <Header/>
         <Nav/>
         <About/>
+        <Experience/>
     </>
   )
 }
