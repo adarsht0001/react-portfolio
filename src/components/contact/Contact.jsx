be6796384b1fc853370e8da39cf5605a4df42f70
adarsht00001 <adarsht00001@gmail.com>
2023-02-14 14:27:55 +0530

    email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,37 +1,56 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
+import { MdOutlineEmail } from 'react-icons/md'
 import { BsWhatsapp } from 'react-icons/bs'
+import emailjs from '@emailjs/browser';
+import env from "react-dotenv";
+
 
 const Contact = () => {
-  return (
-    <section id="contact">
-        <h5>GEt In Touch</h5>
-        <h2>Contact Me</h2>
-        <div className="container contact_container">
-            <div className="contact_options">
-                <article className="contact_option">
-                    <MdOutlineEmail className='contact_option-icon'/>
-                   <h4>Email</h4>
-                   <h5>test@gmail.com</h5>
-                   <a href="mailto:test@gmail.com" target="_blank" rel="noreferrer">Send a message</a> 
-                </article>
-                <article className="contact_option">
-                    <BsWhatsapp className='contact_option-icon'/>
-                   <h4>WhatsApp</h4>
-                   <h5>+</h5>
-                   <a href="https://api.whatsapp.com" target="_blank" rel="noreferrer">Send a message</a> 
-                </article>
+    console.log(env);
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(env.SERVICE_ID, 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+
+    return (
+        <section id="contact">
+            <h5>GEt In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className="container contact_container">
+                <div className="contact_options">
+                    <article className="contact_option">
+                        <MdOutlineEmail className='contact_option-icon' />
+                        <h4>Email</h4>
+                        <h5>test@gmail.com</h5>
+                        <a href="mailto:test@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+                    </article>
+                    <article className="contact_option">
+                        <BsWhatsapp className='contact_option-icon' />
+                        <h4>WhatsApp</h4>
+                        <h5>+</h5>
+                        <a href="https://api.whatsapp.com" target="_blank" rel="noreferrer">Send a message</a>
+                    </article>
+                </div>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name='name' placeholder='Your Full Name' required />
+                    <input type="email" name='email' placeholder='Your Email' required />
+                    <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                </form>
             </div>
-            <form action="">
-                <input type="text" name='name' placeholder='Your Full Name' required/>
-                <input type="email" name='email' placeholder='Your Email' required/>
-                <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
-                <button type='submit' className='btn btn-primary'>Send Message</button>
-            </form>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Contact
