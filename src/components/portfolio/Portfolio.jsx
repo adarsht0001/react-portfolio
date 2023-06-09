02dc4afab6cebf1a82282e71d4fc83b55e45a490
adarsht0001 <adarsht00001@gmail.com>
2023-06-10 04:35:16 +0530

    [update] [rettier added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import "./portfolio.css";
-import eiser from "../../assets/Screenshot from 2023-06-10 03-07-22.png";
-import cambiame from "../../assets/Screenshot from 2023-06-10 03-18-23.png";
-import netflix from "../../assets/Screenshot from 2023-06-10 03-34-10.png";
+import React from 'react';
+import './portfolio.css';
+import eiser from '../../assets/Screenshot from 2023-06-10 03-07-22.png';
+import cambiame from '../../assets/Screenshot from 2023-06-10 03-18-23.png';
+import netflix from '../../assets/Screenshot from 2023-06-10 03-34-10.png';
 
 const data = [
   {
     id: 1,
     image: eiser,
-    title: "eiser.tk",
-    Github: "https://github.com/adarsht0001/eiser-ecommerce",
-    demo: "https://eiser.tk/",
+    title: 'eiser.tk',
+    Github: 'https://github.com/adarsht0001/eiser-ecommerce',
+    demo: 'https://eiser.tk/',
     description:
-      "Build an e-commerce project with Node.js, Express Generator, MongoDB, HBS, Twilio, Razorpay, and PayPal for seamless product listings, shopping cart, and secure payment integration",
+      'Build an e-commerce project with Node.js, Express Generator, MongoDB, HBS, Twilio, Razorpay, and PayPal for seamless product listings, shopping cart, and secure payment integration',
   },
   {
     id: 2,
     image: cambiame,
-    title: "cambiame",
-    Github: "https://github.com/adarsht0001/Cambiame",
-    demo: "https://cambiame.site/",
+    title: 'cambiame',
+    Github: 'https://github.com/adarsht0001/Cambiame',
+    demo: 'https://cambiame.site/',
     description:
-      "Build a comprehensive social media web app with React, Node.js, MongoDB, Express, TypeScript, Clean Architecture, AWS S3, MUI, Redux, and Socket.io for seamless user interactions.",
+      'Build a comprehensive social media web app with React, Node.js, MongoDB, Express, TypeScript, Clean Architecture, AWS S3, MUI, Redux, and Socket.io for seamless user interactions.',
   },
   {
     id: 3,
     image: netflix,
-    title: "Netflix Clone",
-    Github: "https://github.com/adarsht0001/netflix",
-    demo: "https://adarsht0001.github.io/netflix/",
+    title: 'Netflix Clone',
+    Github: 'https://github.com/adarsht0001/netflix',
+    demo: 'https://adarsht0001.github.io/netflix/',
     description:
-      "Developed a Netflix clone using React, leveraging the TMDB API and Axios library, to create an immersive and user-friendly web application for browsing, streaming, and discovering a vast collection of movies and TV shows.",
+      'Developed a Netflix clone using React, leveraging the TMDB API and Axios library, to create an immersive and user-friendly web application for browsing, streaming, and discovering a vast collection of movies and TV shows.',
   },
 ];
 
